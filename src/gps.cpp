fa2de4d03139cbbb3682a60cbde8d1718ece0ee6
Nathan Thomas <developer.nathan@hotmail.com>
2025-01-31 20:18:32 +1100

    updated gps module

--- src/gps.cpp
+++ src/gps.cpp
@@ -2,36 +2,103 @@
 // Created by Nathan on 20/12/2024.
 //
 
+/**
+ *  Hardware serial uses built in-UART GPIOs for serial communication
+ *  Software serial emulates a serial port in software, allowing any GPIO pin for serial communication
+ *
+ *  Hardware serial is faster, more reliable and stable.
+ *  Software serial allows for devices with limited UART pins to be used for serial comms,
+ *  but can be unreliable for higher baud rates (>9600)
+ *
+ *  When using hardware serial ports the UART module (or other dedicated hardware peripherals) automatically
+ *  generates the correct signal timings. "Bit-banging" is a software driven method of serial comms where the
+ *  microcontroller manually controls the timing and transmission of data without dedicated hardware support.
+ *  Software serial uses "bit-banging" to allow any GPIO to simulate a UART connection, at the cost of CPU processing.
+ *
+ *  Bit-banging is a software based method of sending serial data without hardware support.
+ */
+
+
+#define NO_SW_SERIAL
 #include <Arduino.h>
 #include <Adafruit_GPS.h>
-#include <SoftwareSerial.h>
 
-// Use Hardware Serial1 on Mega
-Adafruit_GPS GPS(&Serial1);
+#define BAUD_RATE 9600
+#define GPS_SERIAL Serial1  // Set Serial1 for GPS functionality
+
+// Connect GPS module to GPS Serial
+Adafruit_GPS GPS(&GPS_SERIAL);
+
+char nmeaSentence[100]; // buffer to store NMEA sentence
+size_t nmea_index = 0;              // index for the buffer
 
 void setup() {
-    Serial.begin(115200);  // Serial monitor
-    Serial1.begin(9600);   // GPS module on Serial1 (Mega's RX1, TX1)
 
-    GPS.begin(9600);  // Initialize Adafruit GPS with the correct baud rate
+    /**
+     * Serial1 is mapped to RX1 (pin 19) and TX1 (pin 18) - Hardware Serial Ports
+     * The GPS and microcontroller need to communicate at the same BAUD_RATE
+     * This function allocates a buffer in RAM to store incoming data
+     *
+     * This function sets up the hardware serial (RX1 and TX1) at 9600 baud, enabling direct comms with the GPS module
+     */
+    Serial1.begin(9600);    // Initialises
 
-    // OPTIONAL: Configure GPS update rate (1 Hz)
+    /**
+     * Initialises the hardware or software serial port
+     * Returns True on successful hardware init
+     */
+    if (GPS.begin(BAUD_RATE)) {
+        Serial.print("Hardware Initialised");
+    };
+
+    /**
+     * Send commands to determine which format of NMEA sentences are required
+     */
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+
+    /**
+     * For the parsing code to work nicely and have time to sort through the data, and print it out,
+     * Adafruit doesn't suggest using anything higher than 1 Hz
+     */
     GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
 
-    Serial.println("GPS setup complete.");
+    // Request updates on antenna status
+    GPS.sendCommand(PGCMD_ANTENNA);
+
 }
 
+/**
+ * GPS.read()
+ * GPS.newNMEAreceived() -  new data received?
+ * GPS.parse(GPS.lastNMEA())
+ * GPS.available() - How many bytes are available to read
+ * GPS.write() - sends a byte of data to the GPS module
+ */
+
 void loop() {
-    while (Serial1.available()) {
-        char c = GPS.read();  // Read GPS data
-        if (GPS.newNMEAreceived()) {
-            if (GPS.parse(GPS.lastNMEA())) {  // Successfully parsed sentence
-                Serial.print("Fix: "); Serial.println((int)GPS.fix);
-                Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-                Serial.print("Latitude: "); Serial.print(GPS.latitudeDegrees, 6);
-                Serial.print(", Longitude: "); Serial.println(GPS.longitudeDegrees, 6);
+/**
+ * GPS.read() must be called frequently or the buffer may overflow
+ * Common approach is to use a char buffer to hold an entire sentence, read char until "\n" is presented
+ * NMEA sentence is 82 char long
+ * call newNMEAreceived() after, at least, every 10 calls
+ */
+    if (GPS.available()) {
+        char c = GPS.read();
+
+        /**
+         * if string literals "" are used then the string is automatically null-terminated,
+         * otherwise when entering characters using '' the '\0' null termination must be used
+         */
+        if (c == '\n') {
+            nmeaSentence[nmea_index] = '\0';    // creates a c-style string and allows print to terminal
+            Serial.println(nmeaSentence);
+            nmea_index = 0;
+        } else {
+            if (nmea_index < sizeof(nmeaSentence) - 1) {
+                nmeaSentence[nmea_index] = c;
+                nmea_index++;
             }
         }
     }
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,75 @@
-#include <imu.h>
-#include <heartrate.h>
+//#include <imu.h>
+//#include <heartrate.h>
+
+#define NO_SW_SERIAL
 #include <Arduino.h>
+#include <Adafruit_GPS.h>
 
-static const int heartSensorPin = A0;
+#define BAUD_RATE 9600
+#define GPS_SERIAL Serial1  // Set Serial1 for GPS functionality
 
-// Setup function to initialize the IMU
+
+// Connect GPS module to GPS Serial
+Adafruit_GPS GPS(&GPS_SERIAL);
+
+char nmeaSentence[100]; // buffer to store NMEA sentence
+size_t nmea_index = 0;              // index for the buffer
+
 void setup() {
-    // Start serial communication
-    Serial.begin(115200);
+    Serial.begin(115200);   // Initialises microcontroller
+    Serial1.begin(9600);    // Initialises gps module
 
-    // Wait for Serial to be ready (useful for some boards)
-    while (!Serial) {}
+    if (GPS.begin(BAUD_RATE)) {
+        Serial.print("Hardware Initialised");
+    };
 
-    // setup sensor ranges
-    // setupIMU();
-    delay(1);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+    GPS.sendCommand(PGCMD_ANTENNA);
 }
 
 void loop() {
-    int rawValue = analogRead(heartSensorPin);
+    if (GPS.available()) {
+        char c = GPS.read();
 
-        if (millis() > 3000) {
-            Serial.print("Raw value:      ");
-            Serial.println(rawValue);
-            getHeartRateData();
+        if (c == '\n') {
+            nmeaSentence[nmea_index] = '\0';    // creates a c-style string and allows print to terminal
+            Serial.println(nmeaSentence);
+            nmea_index = 0;
+        } else {
+            if (nmea_index < sizeof(nmeaSentence) - 1) {
+                nmeaSentence[nmea_index] = c;
+                nmea_index++;
+            }
         }
-
-        // updateIMUData();
-        delay(100);
+    }
 }
+
+
+//static const int heartSensorPin = A0;
+//
+//// Setup function to initialize the IMU
+//void setup() {
+//    // Start serial communication
+//    Serial.begin(115200);
+//
+//    // Wait for Serial to be ready (useful for some boards)
+//    while (!Serial) {}
+//
+//    // setup sensor ranges
+//    // setupIMU();
+//    delay(1);
+//}
+//
+//void loop() {
+//    int rawValue = analogRead(heartSensorPin);
+//
+//        if (millis() > 3000) {
+//            Serial.print("Raw value:      ");
+//            Serial.println(rawValue);
+//            getHeartRateData();
+//        }
+//
+//        // updateIMUData();
+//        delay(100);
+//}
